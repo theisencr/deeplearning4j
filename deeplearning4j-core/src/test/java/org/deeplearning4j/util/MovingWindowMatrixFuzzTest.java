afac51fe27d33dc41f8554a7528a147e03b1e3e5
theisencr <theisen.cr@gmail.com>
2015-03-07 20:21:30 -0500

    2nd fuzz test

---
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/util/MovingWindowMatrixFuzzTest.java
@@ -0,0 +1,39 @@
+package org.deeplearning4j.util;
+
+import java.util.List;
+import org.junit.Test;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+import.java.lang.Math;
+
+/**
+ * Created by agibsonccc on 6/11/14.
+ */
+public class MovingWindowMatrixTest {
+
+    @Test
+    public void testMovingWindowFuzz() {
+        Random rand = new Random();
+        rand.setSeed(123456789);
+
+        int max = 2;
+        int min = 10;
+
+        int randomInt = rand.nextInt((max - min) + 1) + min;
+        int randomInt2 = rand.nextInt((max - min) + 1) + min;
+
+        INDArray ones = Nd4j.ones(randomInt, randomInt);
+        MovingWindowMatrix m = new MovingWindowMatrix(ones,randomInt2,randomInt2);
+        List<INDArray> windows = m.windows();
+        assertEquals(Math.max(randomInt,randomInt2),windows.size());
+
+
+    }
+
+
+
+}
