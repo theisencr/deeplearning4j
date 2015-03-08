729bb6729840f4bf8acbbb937d0e5de245f80cd9
theisencr <theisen.cr@gmail.com>
2015-03-07 20:03:53 -0500

    fuzzer actually fuzzes

--- deeplearning4j-core/src/test/java/org/deeplearning4j/eval/EvalFuzzTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/eval/EvalFuzzTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.util.FeatureUtil;
 
+import java.util.Random
+
 /**
  * Created by agibsonccc on 12/22/14.
  */
@@ -13,15 +15,22 @@ public class EvalFuzzTest {
 
     @Test
     public void testEvalFuzz() {
+        Random rand = new Random();
+        rand.setSeed(123456789);
+
+        int randomInt = rand.nextInt((max - min) + 1) + min;
+
         Evaluation eval = new Evaluation();
-        INDArray trueOutcome = FeatureUtil.toOutcomeVector(0,5);
-        INDArray test = FeatureUtil.toOutcomeVector(0,5);
+        INDArray trueOutcome = FeatureUtil.toOutcomeVector(0,randomInt);
+        INDArray test = FeatureUtil.toOutcomeVector(0,randomInt);
         eval.eval(trueOutcome,test);
         assertEquals(1.0,eval.f1(),1e-1);
         assertEquals(1,eval.classCount(0));
 
-        INDArray one = FeatureUtil.toOutcomeVector(1,5);
-        INDArray miss = FeatureUtil.toOutcomeVector(0,5);
+        randomInt = rand.nextInt((max - min) + 1) + min;
+
+        INDArray one = FeatureUtil.toOutcomeVector(1,randomInt);
+        INDArray miss = FeatureUtil.toOutcomeVector(0,randomInt);
         eval.eval(one,miss);
         assertTrue(eval.f1() < 1);
         assertEquals(1,eval.classCount(0));
